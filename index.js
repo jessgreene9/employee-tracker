3df4b4b8b77ac7e191d444931ab151c09b177196
Jess Greene <jess.greene9@gmail.com>
2021-06-16 16:31:23 -0400

    created init function to begin first prompt

--- index.js
+++ index.js
@@ -6,7 +6,70 @@ const connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
 	user: "root",
-	password: "", //
+	password: "", 
 	database: "employeeDB",
 });
 
+const init = () => {
+   
+        inquirer
+            .prompt({
+                name: 'response',
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: [
+                'View all employees',
+                'View employees by departments',
+                'View employees by roles',
+                'View employees by manager',
+                'Add employee',
+                "Remove employee",
+                'Exit'
+            ],      
+        })
+        .then((answer) => {
+            switch (answer.response) {
+                case 'View all employees':
+                    employees();
+                    break;
+                
+                case 'View employees by departments':
+                    empDepartments();
+                    break;
+    
+                case 'View employees by roles':
+                    empRoles();
+                    break;
+                
+                case 'View employees by manager':
+                    empManager();
+                    break;
+
+                case 'Add employee':
+                        addEmployee();
+                        break;    
+
+                 case 'Remove employee':
+                        removeEmployee();
+                        break;           
+    
+                case 'Exit':
+                    
+                    connection.end();
+                    break;
+            }
+        });
+     }
+
+
+
+
+
+
+
+
+
+connection.connect((err) => {
+    if (err) throw err;
+    init();
+})
